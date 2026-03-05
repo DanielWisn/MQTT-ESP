4e24a1a3652c79113faa2a64bda11bb02e5700e2
DanielWisn <146425555+DanielWisn@users.noreply.github.com>
2026-03-05 10:30:52 +0100

    Java MQTT connection basic

--- Java/src/main/java/com/example/java/temperatureSensor/MqttService.java
+++ Java/src/main/java/com/example/java/temperatureSensor/MqttService.java
@@ -1,5 +1,31 @@
 package com.example.java.temperatureSensor;
 
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class MqttService {
 
+    private final TemperatureService temperatureService;
+
+    public MqttService(TemperatureService temperatureService) {
+        this.temperatureService = temperatureService;
+    }
+
+    public void handleMessage(String payload) {
+
+        try {
+            double value = Double.parseDouble(payload);
+
+            Temperature temperature = new Temperature();
+            temperature.setTemperature(value);
+            temperature.setDateTime(LocalDateTime.now());
+
+            temperatureService.updateTemperature(temperature);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid temperature received: " + payload);
+        }
+    }
 }
